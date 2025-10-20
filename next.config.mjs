2d1917c75e7592ff57e971de937f81411965752e
marvin-ctrl <marvin@kaurifutsal.co.nz>
2025-10-20 23:21:26 +1300

    Fix formatting in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
+  typescript: { ignoreBuildErrors: true },
 };
+
 export default nextConfig;
